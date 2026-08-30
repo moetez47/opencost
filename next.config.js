d712f05be12ae9671b9452939befdac819f983d7
moetez47 <moetezbelleh.bedoui@esprit.tn>
2026-08-30 23:57:58 +0100

    Remove dead files: pod.yaml (stale kubectl dump), next.config.js and netlify.toml (unused since move to React Router/Vite + Docker/Helm deploy)

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: ["./node_modules"],
-  },
-};
-
-module.exports = nextConfig;
